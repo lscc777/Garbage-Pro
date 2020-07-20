1d541a97e741d1593bcd0700c659167ab5994e9e
保质期 <345514840@qq.com>
2020-07-20 17:03:59 +0800

    传值成功

--- Image Classifier/GarbageViewController.swift
+++ Image Classifier/GarbageViewController.swift
@@ -7,20 +7,33 @@
 //
 
 import UIKit
+import Foundation
 
 class GarbageViewController: UIViewController {
 
     var image: UIImage?
     var name: String?
     
+    let textMlmodel = TextClassifier()
+    let categroy = ["1": "可回收垃圾", "2": "有害垃圾", "4": "湿垃圾", "8": "干垃圾", "16": "大型垃圾"]
+
     @IBOutlet weak var garbageImage: UIImageView!
     @IBOutlet weak var garbageName: UILabel!
+    @IBOutlet weak var garbageCategroy: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         garbageImage.image = image
         garbageName.text = name
-        // Do any additional setup after loading the view.
+        
+        guard let predictionResult = try? textMlmodel.prediction(text: name!) else {
+            fatalError("Predicting errors!")
+        }
+
+        if let result = categroy[predictionResult.label] {
+            print(result)
+            garbageCategroy.text = result
+        }
     }
     
 
